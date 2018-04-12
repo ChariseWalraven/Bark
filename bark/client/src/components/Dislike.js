5a81b78f9ee79a63979d56b7e31a0e279b185714
Charise Walraven <cherri.walraven@gmail.com>
2018-04-12 19:17:00 +0200

    Refactor

--- bark/client/src/components/Dislike.js
+++ bark/client/src/components/Dislike.js
@@ -1,19 +1,26 @@
 import React, {PureComponent} from 'react'
 import {connect} from 'react-redux'
+import {fetchRandomPhoto} from '../actions/photo'
+
 
 class Button extends PureComponent {
-handleClick(e){
-  e.preventDefault()
-  alert("Dislike clicked!")
-}
+  handleClick = (e) => {
+    e.preventDefault()
+    this.props.fetchRandomPhoto()
+  }
 
   render(){
+    const {photos} = this.props
     return (
-        <button
-          className="fa fa-thumbs-down dislike-button"
-          onClick={this.handleClick}></button>
+        <button className="fa fa-thumbs-down dislike-button" onClick={e => this.handleClick(e)}></button>
     )
   }
 }
 
-export default Button
+const mapStateToProps = function (state) {
+  return {
+    photos: state.photos
+  }
+}
+
+export default connect(mapStateToProps, {fetchRandomPhoto})(Button)
