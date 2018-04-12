bd79df58623b09eb99d7ba9b2c49492c22ef87f4
Charise Walraven <cherri.walraven@gmail.com>
2018-04-12 08:29:14 +0200

    Make api call for random url to dogAPI

---
+++ bark/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const request = require('request');
+
+const app = express()
+app.use(bodyParser())
+//get JSON from API and send to route
+app.get('/', (req, res)=> {
+  request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
+     // returns required JSON object! 
+    res.send(JSON.parse(body).message)
+  })
+})
+
+app.listen(4000, () => console.log("listening from port 4000"))
